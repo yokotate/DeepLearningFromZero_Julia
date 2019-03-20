96c4e75896ce9c67ebce2090e02ab2fc6f0c0844
yokotate <yokosekazuya@yahoo.co.jp>
2019-03-20 15:34:52 +0900

    LossFunctionをModuleに追加

--- src/DeepLearningFromZero_Julia.jl
+++ src/DeepLearningFromZero_Julia.jl
@@ -1,6 +1,21 @@
 module DeepLearningFromZero_Julia
 
-export Sigmoid,Step,ReLU,Softmax,Identity_function
-include("ActivationFunction.jl")
+# 関数指定
+export 
+    # 活性化関数
+    Sigmoid,
+    Step,
+    ReLU,
+    Softmax,
+    Identity_function,
+    
+    # 損失関数
+    mean_squad_error,
+    cross_entropy_error
 
-end # module
+# ファイル指定
+include("ActivationFunction.jl")
+include("LossFunction.jl")
+
+end
+
