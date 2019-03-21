8ed32a8dc6d70fdbf690cde5923febc6242c39c4
yokotate <yokosekazuya@yahoo.co.jp>
2019-03-22 08:49:30 +0900

    無駄な部分を削除

--- src/LossFunction.jl
+++ src/LossFunction.jl
@@ -1,13 +1,12 @@
 # 二乗和誤差
 function mean_squad_error(y, t)
-    a = y .- t
-    a = a.^2
     return 0.5 * sum((y .- t).^2)
 end
 
 # 交差エントロピー誤差
 function cross_entropy_error(y, t)
-    return -sum(t .* log.(y .+ eps())) / size(y,1)
+	delta = eps(7.0)
+    return -sum(t .* log.(y .+ delta))
 end
 
 
